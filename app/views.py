283f1b35c0e297de143a3e00ce79111b30532622
Luvinahlc <908842984@qq.com>
2016-08-09 20:27:51 +0800

    use vis.js for timeline charts

--- app/views.py
+++ app/views.py
@@ -3,37 +3,11 @@ from app import app
 from random import choice
 import pyodbc
 
-Color = ['red', 'green', 'orange', 'magenta', 'default']
-
-def initData():
-    cnxn = pyodbc.connect('Driver={SQL Server Native Client 11.0};Server=tcp:t-luhua.database.windows.net,1433;Database=projectInfoDB;Uid=t-luhua@t-luhua;Pwd={Luvina&921109};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = cnxn.cursor()
-    cursor.execute("use projectInfoDB")
-    cursor.execute("select * from ProjectInfo")
-    rows = cursor.fetchall()
-    data = []
-    for row in rows:
-        content = row.endDate + '--' + row.name_P + '--' + row.milestone
-        
-        lineData = {#'start': row.startDate,
-                    #'end': row.endDate,
-                    'start': row.endDate,
-                    'content': content,
-                    'className': choice(Color)
-                    }
-        data.append(lineData)
-    cursor.close()
-    cnxn.close()
-    return data
-
-
-
 @app.route('/')
 @app.route('/index')
 def index():
-    initalData = initData()
     return render_template("index.html",
-        title = 'Home', initalData = initalData)
+        title = 'Home')
 
 
 @app.route('/get')
